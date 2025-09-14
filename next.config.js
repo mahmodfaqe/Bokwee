e48086bc56c671d81eb80bae1f72445883e075ca
bokwe <mahmodfaqe2@gmail.com>
2025-09-14 04:18:40 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
 module.exports = {
   assetPrefix: isGithubActions ? '/your-repo-name/' : '',
-  basePath: isGithubActions ? '/your-repo-name' : '',
+  basePath: isGithubActions ? '/components/TravelInRanya.tsx' : '',
   output: 'export',
   images: {
     unoptimized: true,
